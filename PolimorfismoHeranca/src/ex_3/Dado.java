0404afff7f75cc5b189a301267d78c4808658126
Matheus Gomes <mgrspeed@gmail.com>
2021-11-18 19:36:04 -0300

    Polimorfismo e Herança

---
+++ PolimorfismoHeranca/src/ex_3/Dado.java
@@ -0,0 +1,15 @@
+package ex_3;
+
+public class Dado {
+	
+	public String toString() {
+		
+			return	"----------Menu----------"
+				+ "\n1- Cadastrar novo produto"
+				+ "\n2- Remover produto"
+				+ "\n3- Listar produtos"
+				+ "\n4 - Atualizar um produto"
+				+ "\n0 - Sair do programa";
+	}
+
+}
